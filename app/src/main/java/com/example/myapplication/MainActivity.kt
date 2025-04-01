0b599fa79eaec3d0b1d91ee8c73eb3769e959e88
giannhsker <giannhsker3@gmail.com>
2025-04-01 11:01:54 +0300

    01/04

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.myapplication
 
+import android.media.tv.TvView
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvMoves: TextView
+    private lateinit var tvPairs: TextView
+
+//    private var boardSize
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        rvBoard = findViewById(R.id.rvBoard)
+        tvMoves = findViewById(R.id.tvMoves)
+        rvBoard.adapter = MemoryBoardAdapter(this,8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2)
+
     }
+
 }
